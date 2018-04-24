d635233bc09f516f3478c2a1e9a31a55e69638f4
Krzysztof Wszelaki <krzys_wszel@interia.eu>
2018-04-24 21:32:15 +0200

    add navbar

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,84 +1,75 @@
- $.jribbble.getShotsByPlayerId('krzys_wszel', function (playerShots) {
-     var html = [];
-
-     //PORTFOLIO SETUP
-
-     $.each(playerShots.shots, function (i, shot) {
-         html.push('<li><a href="' + shot.url + '">');
-         html.push('<img src="' + shot.image_teaser_url + '" ');
-         html.push('alt="' + shot.title + '"></a>');
-         html.push('<h3><a href="' + shot.url + '">' + shot.title + '</h3>');
-         html.push('<div class="likecount"><span class="icon-heart"></span> ' + shot.likes_count + '</div>');
-         html.push('<div class="commentcount"><span class="icon-bubbles"></span> ' + shot.comments_count + '</a></li></div>');
-     });
-
-     $('#shotsByPlayerId').html(html.join(''));
- }, {
-     page: 1,
-     per_page: 9
- });
+//========================
+//CUSTOM SCROLLBAR
+//========================
+$("html").niceScroll({
+    mousescrollstep: 70,
+    cursorcolor: "#ea9312",
+    cursorwidth: "15px",
+    cursorborderradius: "10px",
+    cursorborder: "none",
+});
 
 
- //SCROLLBAR  
-
- $("html").niceScroll({
-     mousescrollstep: 50,
-     cursorcolor: "#ea9312",
-     cursorwidth: "10px",
-     cursorborderradius: "10px",
-     cursorborder: "none",
- });
-
-
- //SMOOTHSCROLL
-
- $(function () {
-     $('a[href*=#]:not([href=#])').click(function () {
-         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-             var target = $(this.hash);
-             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-             if (target.length) {
-                 $('html,body').animate({
-                     scrollTop: target.offset().top
-                 }, 500);
-                 return false;
-             }
-         }
-     });
- });
+//========================
+//SMOOTHSCROLL
+//========================
+$(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
 
 
- //NAVBAR
+//========================
+//NAVBAR
+//========================
+(function ($) {
+    $(document).ready(function () {
 
- (function ($) {
-     $(document).ready(function () {
-         $(".navbar").hide();
-         $(function () {
-             $(window).scroll(function () {
-                 if ($(this).scrollTop() > 40) {
-                     $('.navbar')
-                         .removeClass('animated fadeOutUp')
-                         .addClass('animated fadeInDown')
-                         .fadeIn();
-                 } else {
-                     $('.navbar')
-                         .removeClass('animated fadeInDown')
-                         .addClass('animated fadeOutUp')
-                         .fadeOut();
-                 }
-             });
-         });
+        // hide .navbar first
+        $(".navbar").hide();
 
-     });
- }(jQuery));
+        // fade in .navbar
+        $(function () {
+            $(window).scroll(function () {
 
+                // set distance user needs to scroll before we start fadeIn
+                if ($(this).scrollTop() > 40) {
+                    $('.navbar')
+                        .removeClass('animated fadeOutUp')
+                        .addClass('animated fadeInDown')
+                        .fadeIn();
 
- //icon hover effect
+                } else {
+                    $('.navbar')
+                        .removeClass('animated fadeInDown')
+                        .addClass('animated fadeOutUp')
+                        .fadeOut();
+                }
+            });
+        });
 
- $('#services img').hover(
-     function () {
-         $(this).addClass('animated pulse')
-     },
-     function () {
-         $(this).removeClass('animated pulse')
-     })
+    });
+}(jQuery));
+
+//========================
+//icon hover effect
+//========================
+$('#services img').hover(
+        function () {
+            $(this).addClass('animated pulse')
+        },
+        function () {
+            $(this).removeClass('animated pulse')
+        })
+
+   
