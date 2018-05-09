c0f3c503011334adca650610e9fc28b5d95d7692
Krzysztof Wszelaki <krzys_wszel@interia.eu>
2018-05-09 21:20:56 +0200

    change colors

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,7 +3,7 @@
 //========================
 $("html").niceScroll({
     mousescrollstep: 70,
-    cursorcolor: "#ea9312",
+    cursorcolor: "#76c2af",
     cursorwidth: "15px",
     cursorborderradius: "10px",
     cursorborder: "none",
@@ -17,16 +17,16 @@ $(function () {
     $('a[href*=#]:not([href=#])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+           target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
                 $('html,body').animate({
                     scrollTop: target.offset().top
                 }, 1000);
                 return false;
             }
-        }
+      }
     });
-});
+ });
 
 
 //========================
