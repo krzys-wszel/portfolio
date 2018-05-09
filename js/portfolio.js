3d06f0de107fec7773b4bb5f2f46742f1a8f8d0f
Krzysztof Wszelaki <krzys_wszel@interia.eu>
2018-05-09 18:57:00 +0200

    finish

--- js/portfolio.js
+++ js/portfolio.js
@@ -64,46 +64,14 @@ $(function () {
 //========================
 //icon hover effect
 //========================
-$('#hero').hover(
-    function () {
-        $(this).addClass('animated pulse')
-    },
-    function () {
-        $(this).removeClass('animated pulse')
-    });
+
 $('#services img').hover(
     function () {
         $(this).addClass('animated pulse')
     },
     function () {
         $(this).removeClass('animated pulse')
-    });
-
-$('#skills ').hover(
-    function () {
-        $(this).addClass('animated pulse')
-    },
-    function () {
-        $(this).removeClass('animated pulse')
-    });
-$('#services').hover(
-    function () {
-        $(this).addClass('animated pulse')
-    },
-    function () {
-        $(this).removeClass('animated pulse')
-    });
-$('#about').hover(
-    function () {
-        $(this).addClass('animated pulse')
-    },
-    function () {
-        $(this).removeClass('animated pulse')
-    });
-$('#portfolio').hover(
-    function () {
-        $(this).addClass('animated pulse')
-    },
-    function () {
-        $(this).removeClass('animated pulse')
     })
+
+
+
